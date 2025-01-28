a50b8fbcb33a7c71b3db84a36d7902da8a3f7b55
nico-gao <jtgao99@gmail.com>
2025-01-28 15:14:37 -0600

    Initial commit

---
+++ HTTP-HTML/index.js
@@ -0,0 +1,10 @@
+console.log("hello");
+
+// const button = document.querySelectorAll("button");
+// console.log(button);
+
+function foo() {
+  console.log(this);
+}
+
+foo();
---
+++ HTTP-HTML/note.js
@@ -0,0 +1,25 @@
+/**
+ * GET | POST | PUT | PATCH | DELETE
+ * GET: get data from server
+ * POST: send data to server
+ * PUT/PATCH: update the entire data object/update certain fields
+ * DELETE: remove data
+ *
+ * Status code:
+ *
+ * 100 - informational response
+ * 200 - success
+ * 300 - redirection
+ * 400 - client error
+ * 500 - server error
+ *
+ * DOM (traversal, manipulation)
+ * tree structure
+ * topmost tree nodes: html, body, head
+ * children: direct children, descendents
+ * siblings and parents
+ *
+ * ECMAScript 2015 (es6): syntax standard for javascript
+ * Javascript in browser: ECMAScript + WebAPI
+ * Javacript in node: ECMAScript + NodeAPI
+ */
