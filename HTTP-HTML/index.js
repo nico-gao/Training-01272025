7bc1310db9833f89b62d14e67a30caf7d43b1357
nico-gao <jtgao99@gmail.com>
2025-01-29 15:47:13 -0600

    CSS

--- HTTP-HTML/index.js
+++ HTTP-HTML/index.js
@@ -3,8 +3,23 @@ console.log("hello");
 // const button = document.querySelectorAll("button");
 // console.log(button);
 
-function foo() {
-  console.log(this);
-}
+// function foo() {
+//   console.log(this);
+// }
 
-foo();
+// foo();
+
+const root = document.querySelector("#root");
+const ul = document.querySelector("ul");
+const li = document.querySelector("li");
+
+console.log(root);
+
+const listener = (event) => {
+  console.log(event.currentTarget);
+  alert(`${event.currentTarget.tagName} is clicked`);
+};
+
+root.addEventListener("click", listener);
+// ul.addEventListener("click", listener);
+// li.addEventListener("click", listener);
