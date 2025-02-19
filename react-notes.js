19a06d8a57815c84b42c60c57efd4704a6cfcaf3
nico-gao <jtgao99@gmail.com>
2025-02-19 15:15:41 -0600

    react starter code

---
+++ react-notes.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * React
+ *
+ * component based architecture
+ * divide the UI into reusable components
+ *
+ * virtual DOM
+ * a lightweight copy of the actual DOM
+ * if changes happen, react will first compare the previous version of the virtual DOM with the updated version of the virtual DOM,
+ * find where the changes happen, after that, react will only make the necessary changes to the actual DOM
+ *
+ * one way data flow, parents to children
+ * props (from parent component), state (is managed inside of the component)
+ *
+ * todolist -> todoform
+ *          -> todoitem
+ *
+ *
+ * React is an UI library
+ * library vs framework
+ * more flexible
+ *
+ *
+ * react -> web dev
+ * react native -> mobile dev
+ *
+ *
+ * create-react-app
+ *
+ *
+ * extension
+ * .js  .jsx
+ */
